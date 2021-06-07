9a12effc29b9e30d2fdfca10d1416b525c73b4b0
Wajid2001 <s.w.a.11Wajid@gmail.com>
2021-06-07 16:40:20 +0530

    backend routes created
    ~

---
+++ backend/urls.py
@@ -0,0 +1,15 @@
+from django.urls import  path
+from . import views
+
+
+app_name = "backend"
+
+
+urlpatterns = [
+
+    # These are api routes
+    path('api/account/signin', views.signIn, name="signIn"),
+    path('api/account/signout', views.signOut, name="signOut"),
+    path('api/account/check_status', views.check_authentication_status),
+    path('api/register', views.registerUserApi)
+]
