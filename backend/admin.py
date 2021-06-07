0bba7ea865595950b126babf1713890ef99f7567
Wajid2001 <s.w.a.11Wajid@gmail.com>
2021-06-07 13:09:11 +0530

    Improved Admin
    Created Super User for Main Django Website Administration
    
    1. Created Super User for Main Django Website Administration
    2. Integrated User Model to Django Admin Page

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+class UserAdminView(admin.ModelAdmin):
+    list_display= ('id','first_name', 'last_name', 'username', 'email')
+
+admin.site.register(User, UserAdminView)
