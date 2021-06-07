813750651c97e9c1fa8ceaa7bcc6f700bd5698ea
Wajid2001 <s.w.a.11Wajid@gmail.com>
2021-06-07 13:09:34 +0530

    Created backend for sign in page

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,123 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+# This will be helpful for converting a fetch request body to json
+import json
+
+# For signIn and signOut Functionality
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import requires_csrf_token, ensure_csrf_cookie
+
+# For user creation 
+from .models import User
+
+
+# For check_authentication_status
+from django.http import JsonResponse
+
+
+""" 
+                Section Index
+    1.  Sign In and Out functionality, with json
+"""
+
+
+""" 
+    1.  Sign In and Out functionality, with json
+"""
+
+# This will sign in the user
+# This  decorator will ensure that user is logged in from the safest way possible
+@requires_csrf_token
+def signIn(request):
+    if request.method == "POST":
+        
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+
+            # If user want is redirected from some page like dashboard
+            if "next" in request.POST:
+                return redirect(request.POST["next"])
+            else:
+                return HttpResponseRedirect(reverse("frontend:index"))
+        else:
+            return render(request, "frontend/signin.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return HttpResponseRedirect(reverse("frontend:signIn_View"))            
+
+
+# This will signOut the user
+@login_required
+def signOut(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("frontend:index"))
+
+
+# This will check if the user is signed in or not 
+# And send a JSON reposnse
+def check_authentication_status(request):
+    if request.user.is_authenticated:
+        return JsonResponse({"authenticated" : True,
+                             "username" : request.user.username,
+                             "email": request.user.email,
+                             "firstName": request.user.first_name,
+                             "lastName": request.user.last_name,
+                             "designation":request.user.get_designation_display()} , status=200)
+    else:
+        return JsonResponse({"authenticated" : False} , status=200)
+
+
+
+# This will allow only high level user's to create account
+@requires_csrf_token
+@ensure_csrf_cookie
+def registerUserApi(request):
+
+    if request.method == "POST":
+
+        print(f"\n\n\n{request.POST}\n\n\n")
+
+        # Checking if the user is authenticated for doing this proccess
+        if request.user.is_authenticated and request.user.designation in ["A","P","T","St"]:
+            
+            print(f"\n\n\n{request.POST}\n\n\n")
+            data = json.loads(request.body)
+            username = data['username']
+            # username = request.POST["username"]
+            first_name = data["first_name"]
+            last_name = data["last_name"]
+            designation = data["designation"]
+            email = data["email"]
+            password = data["password"]
+
+            # Checking if the username already exists
+            if len(User.objects.filter(username=username)) != 0:
+                return JsonResponse({"Error" : "Username already exists."} , status=409)
+
+            # Creating the user
+            else:
+                user = User.objects.create_user(username, email, password)
+                user.first_name = first_name
+                user.last_name = last_name
+                user.designation = designation
+                user.save()
+
+            return JsonResponse({"Account" : True} , status=201)
+
+        # If user is not authorized to create accounts
+        else:
+            return JsonResponse({"Account" : "Not authorized"} , status=401)
+    else:
+        return HttpResponseRedirect(reverse("frontend:dashboard"))     
+
+
