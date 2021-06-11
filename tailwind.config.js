4beadc1c507017a8e01e5320436f5bd5a52cc767
Wajid2001 <s.w.a.11Wajid@gmail.com>
2021-06-11 23:13:18 +0530

    Debug mode for tailwind css
    Added Scripts for Production mode and development mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-	purge: {
-		enabled: true,
-		content: ["./React_Files/**/*.js"],
-	},
+	// This is for Production
+	// purge: {
+	// 	enabled: true,
+	// 	content: ["./React_Files/**/*.js"],
+	// },
+
+	// This is for Development
+	purge: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {},
