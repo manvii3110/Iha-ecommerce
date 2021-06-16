0e4e1b449dc11496d019df3ac04b7f999128de46
Wajid2001 <s.w.a.11Wajid@gmail.com>
2021-06-16 06:08:31 +0530

    Revert "Created Header for the home page"
    This reverts commit 66d63f8af4cd773cf77ff81ade77538a350a8680.

--- React_Files/Home.js
+++ React_Files/Home.js
@@ -1,12 +1,9 @@
-import React, { useEffect, useState, lazy, Suspense } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 require("./static/index.scss");
 
-const Header = lazy(() => import("./Components/Header"));
-const Footer = lazy(() => import("./Components/Footer"));
-
-const Temp = () => {
+const Home = () => {
 	const [username, setName] = useState("You Need to Sign in");
 
 	useEffect(() => {
@@ -21,15 +18,9 @@ const Temp = () => {
 	}, []);
 	return (
 		<div>
-			<Suspense fallback={<div className='loading'></div>}>
-				<Header />
-			</Suspense>
 			<h1 class='font-bold text-7xl text-center mx-5'>Hi, {username}</h1>
-			<Suspense fallback={<div className='loading'></div>}>
-				<Footer />
-			</Suspense>
 		</div>
 	);
 };
 
-ReactDOM.render(<Temp />, document.getElementById("root"));
+ReactDOM.render(<Home />, document.getElementById("root"));
