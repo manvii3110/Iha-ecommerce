eb014e1253ce19a64f87217c651b60ae4eb5d81f
Wajid2001 <s.w.a.11Wajid@gmail.com>
2021-06-19 16:35:33 +0530

    Register backend fix

--- backend/views.py
+++ backend/views.py
@@ -127,6 +127,7 @@ def registerUserApi(request):
             user.first_name = first_name
             user.last_name = last_name
             user.save()
+            login(user)
             return JsonResponse({"account" : True} , status=201)
 
     else:
