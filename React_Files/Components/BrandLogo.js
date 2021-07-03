187523e45e6cf4f171722c76c3bb34482b9188b1
Wajid2001 <s.w.a.11Wajid@gmail.com>
2021-07-03 15:42:38 +0530

    Changed brand Logo

--- React_Files/Components/BrandLogo.js
+++ React_Files/Components/BrandLogo.js
@@ -2,12 +2,12 @@ import React from "react";
 
 require("../static/index.scss");
 
-export const BrandLogoImg = () => {
+export const BrandLogoImg = (props) => {
 	return (
 		<>
 			<img
-				className='h-12	 w-12'
-				src='./static/frontend/svg/ihaLogo.svg'
+				className={`h-12 w-12 ${props.className}`}
+				src='./static/frontend/img/Iha Unnathi logo Croped.jpeg'
 				alt='Iha Ecommerce Logo'
 			/>
 		</>
@@ -21,7 +21,7 @@ export const BrandLogoCircle = () => {
 			className='flex mx-auto w-max transition flex px-1 py-1 rounded-full bg-transparent hover:bg-white active:bg-gray-100 border border-white hover:border-gray-200 active:border-gray-500 shadow-none hover:shadow active:shadow-md'
 			title='Goto Iha Ecommerce'
 		>
-			<BrandLogoImg />
+			<BrandLogoImg className='rounded-full' />
 		</a>
 	);
 };
@@ -30,9 +30,6 @@ const BrandLogo = () => {
 	return (
 		<a href='./' className='flex ml-2 sm:ml-0' title='Goto Iha Ecommerce'>
 			<BrandLogoImg />
-			<div className='hidden md:block font-normal text-gray-900 my-auto ml-1 mb-2'>
-				Ship it Free
-			</div>
 		</a>
 	);
 };
