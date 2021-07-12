cfffa8b62e136b73a7534c86f704ecc62f1ac506
Wajid2001 <s.w.a.11Wajid@gmail.com>
2021-07-12 20:21:02 +0530

    Update Cards.js

--- React_Files/Cards.js
+++ React_Files/Cards.js
@@ -1,1 +1,21 @@
-// Todo
+import React from "react";
+import ReactDOM from "react-dom";
+
+require("./static/index.scss");
+
+const Card_v = () => {
+	return <div>This is card vertical</div>;
+};
+const Card_h = () => {
+	return <div>This is card horizontal</div>;
+};
+const Cards = () => {
+	return (
+		<div>
+			These are cards
+			<Card_h /> <Card_v />
+		</div>
+	);
+};
+
+ReactDOM.render(<Cards />, document.getElementById("root"));
