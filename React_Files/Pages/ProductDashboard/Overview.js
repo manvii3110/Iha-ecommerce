efcabf8491731aa0ba03f96c3f31c4161a7deecf
Manvi <msinghal3102@gmail.com>
2021-07-28 14:39:29 +0530

    Added chart in Overview

--- React_Files/Pages/ProductDashboard/Overview.js
+++ React_Files/Pages/ProductDashboard/Overview.js
@@ -1,7 +1,165 @@
 import React from "react";
+import { 
+    LineChart, 
+    ResponsiveContainer, 
+    Legend, Tooltip, 
+    Line, 
+    XAxis, 
+    YAxis, 
+    CartesianGrid 
+} from 'recharts'; 
+const pdata = [ 
+    { 
+        date: '0',  
+        time: 2 
+    },
+    { 
+        date: 'Jun 27',  
+        time: 0
+    }, 
+    { 
+        date: 'Jun 29', 
+        time: 0
+    }, 
+    { 
+        date: 'Jul 01',  
+        time: 1
+    }, 
+    { 
+        date: 'Jul 05', 
+        time: 0
+    }, 
+    { 
+        date: 'Jul 07',  
+        time: 0
+    }, 
+    { 
+        date: 'Jul 09', 
+        time: 2
+    }, 
+    { 
+        date: 'Jul 11', 
+        time: 1
+    }, 
+     
+    { 
+        date: 'Jul 15', 
+        time: 0
+    }, 
+    { 
+        date: 'Jul 17', 
+        time: 0
+    }, 
+    { 
+        date: 'Jul 19', 
+        time: 1
+    }, 
+    { 
+        date: 'Jul 21',  
+        time: 1 
+    }, 
+    { 
+        date: 'Jul 23', 
+        time: 2
+    }, 
+    { 
+        date: 'Jul 25', 
+        time: 3
+    }, 
+    { 
+        date: 'Jul 27', 
+        time: 0
+    },
+    { 
+        date: 'Jul 28', 
+        time: 0
+    },
+    { 
+        date: 'Jul 29', 
+        time: 1
+    },
+    { 
+        date: 'Jul 31', 
+        time: 1
+    },
+    
+    { 
+        date: 'Aug 03', 
+        time: 0
+    },
+    { 
+        date: 'Aug 04', 
+        time: 0
+    },
+    { 
+        date: 'Aug 05', 
+        time: 2
+    },
+    { 
+        date: 'Aug 06', 
+        time: 0
+    }, 
+    { 
+        date: 'Aug 08', 
+        time: 0
+    },
+    { 
+        date: 'Aug 10', 
+        time: 2
+    },
+    { 
+        date: 'Aug 12', 
+        time: 0
+    },
+    { 
+        date: 'Aug 14', 
+        time: 4
+    },
+    { 
+        date: 'Aug 16', 
+        time: 0
+    },
+    { 
+        date: 'Aug 17', 
+        time: 0
+    },
+    { 
+        date: 'Aug 18', 
+        time: 0
+    },
+    { 
+        date: 'Aug 20', 
+        time: 1
+    },
+    { 
+        date: 'Aug 22', 
+        time: 1
+    },
+    { 
+        date: 'Aug 24', 
+        time: 0
+    },
+]; 
 
 const Overview = () => {
-	return <div>Overview</div>;
-};
-
-export default Overview;
+			return ( 
+        <> 
+            <ResponsiveContainer width="100%" aspect={4}> 
+                <LineChart data={pdata} margin={{ right: 40 }}
+                > 
+                    <CartesianGrid /> 
+                    <XAxis dataKey="date" 
+                        interval={'preserveStartEnd'} /> 
+                    <YAxis dataKey="time"></YAxis> 
+                    <Legend /> 
+                    <Tooltip /> 
+                    
+                    <Line dataKey="time"
+                        stroke="orange" activeDot={{ r: 8 }} /> 
+                </LineChart> 
+            </ResponsiveContainer> 
+        </> 
+    );  
+}
+  
+export default Overview;  
---
+++ frontend/static/frontend/js/ProductDashboard/runtime/React_Files_Pages_ProductDashboard_Blocked_js.js
@@ -0,0 +1,22 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkiha_ecommerce"] = self["webpackChunkiha_ecommerce"] || []).push([["React_Files_Pages_ProductDashboard_Blocked_js"],{
+
+/***/ "./React_Files/Pages/ProductDashboard/Blocked.js":
+/*!*******************************************************!*\
+  !*** ./React_Files/Pages/ProductDashboard/Blocked.js ***!
+  \*******************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\n\nvar Blocked = function Blocked() {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", null, \"Blocked\");\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Blocked);\n\n//# sourceURL=webpack://iha-ecommerce/./React_Files/Pages/ProductDashboard/Blocked.js?");
+
+/***/ })
+
+}]);
